455aea162c625ee13e78147a2d2baea3a9d71047
Torjusam <torjusrfx@gmail.com>
2024-02-18 16:01:04 +0100

    Rework 💯 pt.2 (WIP)
    1. Fixed employee selection (uses employee object instead of emplyoee ID. Reduces amount of code).
    2. employeeContext sorts the list of employees by clockedIn stauts when its initalized.
    3. Pass down "selectedEmployee" state setter from employeeShiftList -> buttonsBelowTable -> clockInOutButton - this updates the state refreshing the table once a clock in / out has been done

--- pages/crudTest.tsx
+++ pages/crudTest.tsx
@@ -1,16 +1,17 @@
-//Author: Torjus A.M
+// Author: Torjus A.M
 import React from "react";
 import EmployeeForm from '../components/CRUD-page/employeeForm';
 import GoToIndexButton from '../components/Buttons/redirectToIndexButton';
+import Square from "../components/employeeTerminal/square";
 
 const CrudPage: React.FC = () => {
 
     return (
-        <div className="flex flex-col items-center justify-center mt-8">
+        <Square>
             <EmployeeForm />
             <GoToIndexButton />
-            </div>
-            );
+        </Square>
+    );
 }
 
 export default CrudPage;
