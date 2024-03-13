db094a36116210b85c669f7f4994d108e8295187
Torjusam <torjusrfx@gmail.com>
2024-03-13 17:28:23 +0100

    Searchbar functionality
    The searchbar now works as intended. It updates the sortedEmployees state variable from the global context, and sorts the list when its cleared. Still need to update the employeecards styling to change size when being searched, but thats minor.

--- src/context/employeeContext.tsx
+++ src/context/employeeContext.tsx
@@ -13,6 +13,7 @@ interface EmployeeContextProps {
     employees: Employee[];
     setEmployees: React.Dispatch<React.SetStateAction<Employee[]>>;
     sortedEmployees: Employee[];
+    setSortedEmployees: React.Dispatch<React.SetStateAction<Employee[]>>;
 }
 
 // Shared context for the employee state (initalized with empty placeholder props)
@@ -21,39 +22,29 @@ export const EmployeeContext = createContext<EmployeeContextProps | null>(null);
 // Custom provider provides children components with state and updater function for the employee state.
 export default function EmployeeContextProvider({ children }: { children: React.ReactNode }) {
     const [employees, setEmployees] = useState<Employee[]>([]);
+    const [sortedEmployees, setSortedEmployees] = useState<Employee[]>([]);
 
     // Initialize the employees array when the component mounts.
     useEffect(() => {
         const initializeEmployees = async () => {
             const fetchedEmployees = await fetchEmployees();
-            
+
             setEmployees(fetchedEmployees);
         };
         initializeEmployees();
     }, []);
-
-    /* useMemo hook for creating a memoized version of employes array, that is sorted based on isClockedIn status
-    and last check time. This sorted array is recalculated only when the employees array changes. */
-    const sortedEmployees = useMemo(() => {
-        return employees.sort((a, b) => {
-            if (a.isClockedIn && !b.isClockedIn) {
-                return -1;
-            } else if (!a.isClockedIn && b.isClockedIn) {
-                return 1;
-            } else {
-                // When the clockedIn-status of a & b are the same, sort based on last check time.
-                const aLastCheckTime = a.isClockedIn ? new Date(a.lastCheckIn).getTime() : new Date(a.lastCheckOut).getTime();
-                const bLastCheckTime = b.isClockedIn ? new Date(b.lastCheckIn).getTime() : new Date(b.lastCheckOut).getTime();
-                return bLastCheckTime - aLastCheckTime;
-            }
-        });
-        // Dependency array specifies to only update when employees array is changed.
+    
+    // Sorts employee array when its updated.
+    useEffect(() => {
+        const sortedArray = sortEmployees(employees);
+        setSortedEmployees(sortedArray);
     }, [employees]);
 
     const value = {
         employees,
         setEmployees,
         sortedEmployees,
+        setSortedEmployees,
     };
 
     // Wraps children so that any child component can access the employee state.
@@ -72,3 +63,19 @@ export function useEmployeeContext() {
     }
     return context;
 }
+
+// Function to sort the employees array by 1: status and 2: recency of clock-operations.
+export function sortEmployees (employees: Employee[]) {
+    // Copy of employees array to avoid mutating the original.
+    return [...employees].sort((a, b) => {
+        if (a.isClockedIn && !b.isClockedIn) {
+            return -1;
+        } else if (!a.isClockedIn && b.isClockedIn) {
+            return 1;
+        } else {
+            const aLastCheckTime = a.isClockedIn ? new Date(a.lastCheckIn).getTime() : new Date(a.lastCheckOut).getTime();
+            const bLastCheckTime = b.isClockedIn ? new Date(b.lastCheckIn).getTime() : new Date(b.lastCheckOut).getTime();
+            return bLastCheckTime - aLastCheckTime;
+        }
+    });
+};
--- src/features/searchBar/components/searchbar.tsx
+++ src/features/searchBar/components/searchbar.tsx
@@ -1,26 +1,52 @@
 /*
     Author: Torjus A.M
-    Searchbar to search employees for the navbar on the homepage.
+    Searchbar to search employees on the homepage. Updates the sortedEmployees array from the global context. 
+    This array is the one used to display the employees on the frontpage: src/components/frontpageTable/employeeTable.tsx
 */
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useEffect, useState, FC } from 'react';
 import Search from '../../../assets/search.svg';
+import { useEmployeeContext, sortEmployees } from '../../../context/employeeContext';
+import { Employee } from '../../../lib/employee';
 import styles from './searchbar.module.css';
 
-interface SearchBarFormProps {
+const SearchBarForm: FC = () => {
+    const { employees, setSortedEmployees } = useEmployeeContext();
+    const [searchInput, setSearchInput] = useState("");
+    // Boolean to keep track of if search returns no results.
+    const [emptySearch, setEmptySearch] = useState(false);
 
-}
+    // Hook updates the sortedEmployees array by filtering it on the search input.
+    useEffect(() => {
+        if (searchInput.length > 0 || searchInput ) {
+            const searchResults = employees.filter((employee: Employee) =>
+                employee.name.toLowerCase().includes(searchInput.toLowerCase())
+            );
+            setSortedEmployees(searchResults);
+            // When a search returns no results, the emptySearch state variable is set to true.
+            setEmptySearch(searchResults.length === 0);
+        } else {
+            const sortedArray = sortEmployees(employees);
+            // When the search input is cleared, the array is reset to its original sorting (by status and recency of clock-operations).
+            setSortedEmployees(sortedArray);
+            setEmptySearch(false);
+        }
+        // Set to re-run everytime the input in the searchbar changes. 
+    }, [searchInput]);
 
-const SearchBarForm: React.FC<SearchBarFormProps> = () => {
-
-    const handleSubmit = (event: React.FormEvent) => {
-        // Prevents the default form submission, which is a page refresh.
+    // Prevents the form from submitting, as we want to handle the search ourselves.
+    const handleSubmit = (event: FormEvent) => {
         event.preventDefault();
+    };
+
+    // Updates search input state variable whenever change is registered.
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearchInput(event.target.value);
     };
 
     return (
         <form onSubmit={handleSubmit} role="search" className={styles.form}>
             <Search className={styles.icon}/>
-            <input id="search" type="search" placeholder="Søk ansatte..." autoFocus required className={styles.input} />
+            <input id="search" type="search" placeholder="Søk ansatte..." autoFocus required className={styles.input} onChange={handleChange} value={searchInput} />
             <button type="submit" className={styles.button}>Søk</button>
         </form>
     );
