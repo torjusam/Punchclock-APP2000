15b0f260387b79b958c6ef3e603b310635e98639
thomashau8 <fainted2k4@gmail.com>
2024-03-22 01:37:56 +0100

    Vaktliste CSS update / placeholder data

--- src/components/employeePage/shiftList.tsx
+++ src/components/employeePage/shiftList.tsx
@@ -8,7 +8,18 @@ interface ShiftListProps {
     employee: Employee;
 }
 
+// Temp data, bytt med reell data fra backend
+const tempData = [
+    {id: '1', date: '19', dayOfWeek: 'Man', startTime: '16:00', endTime: '19:00'},
+    {id: '2', date: '20', dayOfWeek: 'Man', startTime: '16:00', endTime: '19:00'},
+    {id: '3', date: '21', dayOfWeek: 'Man', startTime: '16:00', endTime: '19:00'},
+    {id: '4', date: '21', dayOfWeek: 'Man', startTime: '16:00', endTime: '19:00'},
+    {id: '4', date: '21', dayOfWeek: 'Man', startTime: '16:00', endTime: '19:00'},
+    {id: '4', date: '21', dayOfWeek: 'Man', startTime: '16:00', endTime: '19:00'},
+]
+
 const ShiftList: FC<ShiftListProps> = ({ employee }) => {
+    const shifts = tempData; // Bytt med reell data
 
     useEffect(() => {
         // Add your code here
@@ -35,10 +46,21 @@ const ShiftList: FC<ShiftListProps> = ({ employee }) => {
                 <hr />
             </div>
             <div className={styles.moduleContent}>
-                <h1>Put content here</h1>
-            </div>
+                {shifts.map((shift) => (
+                  <div key={shift.id} className={styles.shiftItem}>
+                    <div className={styles.shiftDateContainer}>
+                        <div className={styles.shiftDate}>{shift.date}</div>
+                        <div className={styles.dayOfWeek}>{shift.dayOfWeek}</div>
+                    </div>
+                    <div className={styles.shiftTime}>{`${shift.startTime} - ${shift.endTime}`}</div>
+                    <div className={styles.shiftExpandIcon}>
+                        <Expand className={styles.expandIcon}/>
+                   </div>
+              </div>
+            ))}
         </div>
-    );
+    </div>
+   );
 };
 
 export default ShiftList;
