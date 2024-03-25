0ff3357fc3729eeae8891331077206621c1dab23
Torjusam <torjusrfx@gmail.com>
2024-03-25 17:43:10 +0100

    Signin-form styles

--- src/components/authentication/signInPage.tsx
+++ src/components/authentication/signInPage.tsx
@@ -7,17 +7,17 @@
         Protect api routes
         Github provider
 */
-import React, {FormEvent, useState} from 'react'
+import React, {useState} from 'react'
 import {useRouter} from 'next/router'
 import {signIn} from 'next-auth/react'
-import {useFormik, ErrorMessage} from 'formik';
+import {useFormik} from 'formik';
 import login_validate, {FormValues} from "../../lib/validate"
 import styles from './signInPage.module.css'
 
 
 export default function SignInPage() {
     const router = useRouter()
-    const [loggedIn, setIsLoggedIn] = useState(false)
+    const [statusText, setStatusText] = useState('')
     // formik hook
     const formik = useFormik({
         initialValues: {
@@ -33,49 +33,63 @@ export default function SignInPage() {
             const status = await signIn("credentials", {
                 email: values.email,
                 password: values.password,
-                callbackUrl: "/",
                 redirect: false
             })
 
             // If the user is authenticated, redirect to the home page
             if (status.ok) {
-                router.push('/');
+                await router.push('/');
             } else {
-                console.log(status.error);
+                setStatusText(status.error);
             }
         } catch (error) {
-            console.error(error);
+            setStatusText(error.message);
         }
     }
 
     return (
-        <div>
-            <form onSubmit={formik.handleSubmit}>
-                <div
-                    className={`${styles.inputGroup} ${formik.errors.password && formik.touched.password ? styles.errorMsg : ''}`}>
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                        {...formik.getFieldProps('email')}
-                        required/>
-                </div>
-                {/* formik.errors.email && formik.touched.email ? <span className={styles.errorMsg}>{formik.errors.email as string}</span> : <></> */}
-                <div
-                    className={`${styles.inputGroup} ${formik.errors.password && formik.touched.password ? styles.errorMsg : ''}`}>
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
-                        {...formik.getFieldProps('password')}
-                        required/>
-                </div>
-                <button type="submit">Login</button>
-            </form>
+        <div className={styles.outerContainer}>
+            <div className={styles.formContainer}>
+                <h1>Logg inn</h1>
+                {/* Error message */}
+                {statusText &&
+                    <div className={styles.errorMsg}><h2><span className={styles.strongText}>Feil - </span>{statusText}
+                    </h2></div>}
+                <form onSubmit={formik.handleSubmit}>
+                    <div className={styles.inputGroup}>
+                        <h2>Brukernavn</h2>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
+                            {...formik.getFieldProps('email')}
+                            required
+                            style={{
+                                ...(formik.errors.email && formik.touched.email ? {borderColor: 'red'} : {})
+                            }}
+                        />
+                    </div>
+                    {/* formik.errors.email && formik.touched.email ? <span className={styles.errorMsg}>{formik.errors.email as string}</span> : <></> */}
+                    <div className={styles.inputGroup}>
+                        <h2>Passord</h2>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                            {...formik.getFieldProps('password')}
+                            required
+                            style={{
+                                ...(formik.errors.password && formik.touched.password ? {borderColor: 'red !important'} : {})
+                            }}
+                        />
+                    </div>
+                    <button type="submit" className={styles.submitBtn}>Login</button>
+                </form>
+            </div>
         </div>
     )
 }
