4a1b1852f9b3746a18c7ce23ecd4ca5b2e4cb8c1
Torjusam <torjusrfx@gmail.com>
2024-04-17 12:58:46 +0200

    Test removal of allowed origin

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,19 +12,8 @@ async function middleware(request: Request) {
     const regex = new RegExp('/api/*');
 
     if (regex.test(request.url)) {
-        const allowedOrigins = process.env.NODE_ENV === 'production' ? ['https://app-2000-gruppe20.vercel.app/'] : ['http://localhost:3000'];
         const origin = request.headers.get('origin');
-
-        // Block requests from origins not in the allowedOrigins array.
-        if (origin && !allowedOrigins.includes(origin)) {
-            return new NextResponse(null, {
-                status: 400,
-                statusText: 'Bad request',
-                headers: {
-                    'Content-Type': 'text/plain',
-                },
-            });
-        }
+        console.log('Origin:', origin);
     }
 }
 
