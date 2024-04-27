63e1a701a7391cb261ff549e8ef0d752249800de
GitGudree <117321184+GitGudree@users.noreply.github.com>
2024-04-27 22:23:10 +0200

    added database things

--- src/features/clock-operation/services/extras/clockChange.ts
+++ src/features/clock-operation/services/extras/clockChange.ts
@@ -4,26 +4,29 @@
  * @Author Thomas H
  * @editor Ask I.P.A
  */
-import {Employee} from "../../../../lib/types/employee";
+import Employee from "../../../../utils/employee";
 
 /**
  * Performs a clock-in operation for an employee.
  *
  * @param {Employee} employee - The employee who is clocking in.
- * @returns {Promise<boolean>} - Returns a promise that resolves to true if the operation was successful.
+ * @param date - the chosen date to edit
+ * @param clockIn - 
  * @throws {TypeError} - Throws a TypeError if the employee is already clocked in.
  * @throws {Error} - Throws an Error if the response from the server is not ok.
  */
-export const clockIn = async (employee: Employee, date, clockIn, clockOut) => {
+export const clockChange = async (employee: Employee, date, oldClockIn, oldClockOut, newClockIn, newClockOut) => {
+    /*
     if (employee.isClockedIn)
         return Promise.reject(new TypeError(employee.name + ' er ikke utstemplet!'));
+    */
     try {
         const response = await fetch('/api/clockOperation/clockIn', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({employee, date, clockIn, clockOut}),
+            body: JSON.stringify({employee, date, oldClockIn, oldClockOut, newClockIn, newClockOut}),
         });
         if (!response.ok) {
             throw new Error('Feil ved utstempling!');
