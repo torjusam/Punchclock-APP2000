754b9a4312b4f2343a2bd50bbd6c5d8650c656cb
Torjusam <torjusrfx@gmail.com>
2024-07-01 10:48:32 +0300

    Remove unused files

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-// Config file for Next.js. Webpack function allows you to add custom webpack configuration, in this case rule for .svg files.
 module.exports = {
     webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-  
-      return config;
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
     },
-  };
+};
